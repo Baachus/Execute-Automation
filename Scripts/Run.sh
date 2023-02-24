a4a287a38360dca391c8ade4454502e8deb90658
Baachus <bc.robert@gmail.com>
2023-02-24 12:28:59 -0600

    Added support for reporting artifacts

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -9,8 +9,15 @@ cd "$(dirname "$0")/.."
 
 docker-compose -p "$project" build
 
+
+mkdir -m 777 -p Reports
+
 docker-compose -p "$project" up -d ea_api ea_webapp db selenium-hub firefox chrome edge
 docker-compose -p "$project" up --no-deps ea_test
+
+docker cp ea_test:/src/EATestBDD/LivingDoc.html ./Reports
+echo "Specflow living document report is copied to ./Reports"
+ls -l ./Reports
 
 exit_code=$(docker inspect ea_test -f '{{.State.ExitCode}}')
 
